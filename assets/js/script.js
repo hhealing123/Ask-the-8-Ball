81f629b3a2c6e30caf351a709777f74f3aeeb2c0
rjschroeder <are.jay.schroeder@gmail.com>
2022-08-01 21:21:14 -0500

    initial test of avatar api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-hi
+var requestUrl = 'https://api.multiavatar.com/';
+
+var responseText = document.getElementById('box');
+const image = document.createElement('img');
+
+function getApi(requestUrl) {
+    let avatarId = 'Matteo'
+    fetch('https://api.multiavatar.com/'+JSON.stringify(avatarId)+'.svg')
+        .then(function(response) {
+            console.log(response);
+            image.setAttribute(
+                'src',
+                'https://api.multiavatar.com/'+JSON.stringify(avatarId)+'.svg'
+            )
+            image.setAttribute('height', 50);
+            image.setAttribute('width', 50); 
+        })
+}
+
+responseText.appendChild(image)
+getApi(requestUrl);
