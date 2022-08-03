5b76311ac105e64f784a52548ad685eec597430a
rjschroeder <are.jay.schroeder@gmail.com>
2022-08-02 19:44:46 -0500

    added comments for code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
+//API request URL
 var requestUrl = 'https://api.multiavatar.com/';
 
+//This enteredUser variable will change to contain whatever text is entered into the username box
 var enteredUser = "Matteo"
-var responseText = document.getElementById('box');
+
+//toAttach is wherever on the HTML the avatar container will be appended to
+var toAttach = document.getElementById('box');
+
+//creating the container for avatar picture and user name
 var avatarContainer = document.createElement('div');
+
+//creating text element to store user name, then sets text content to enteredUser
 var avatarNameElement = document.createElement('p');
 avatarNameElement.textContent = enteredUser;
 
+//TEMPORARY BORDER- to show where the container extends
 avatarContainer.style.border = '5px solid pink';
-avatarContainer.style.display = 'inline-flex';
 
+//hardcoded style attribute to display contents of container inline
+avatarContainer.style.display = 'inline-flex';
 avatarNameElement.style.margin = 'auto';
 
 const image = document.createElement('img');
@@ -22,13 +32,17 @@ function getApi(requestUrl) {
                 'src',
                 'https://api.multiavatar.com/'+JSON.stringify(avatarId)+'.svg'
             )
+            //hardcoded image width and height, can be later changed into flex or percentage
             image.setAttribute('height', 50);
             image.setAttribute('width', 50); 
         })
 }
 
+//adding image and name to container
 avatarContainer.appendChild(image);
 avatarContainer.appendChild(avatarNameElement);
-responseText.appendChild(avatarContainer);
+
+//adding container to whatever element is in toAttach
+toAttach.appendChild(avatarContainer);
+
 getApi(requestUrl);
-//Adding a test comment here 
