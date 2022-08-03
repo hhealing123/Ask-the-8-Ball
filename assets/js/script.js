bafe511ee76161d4b2afba282f9807fb59366d80
michaelalexdiaz <107277834+michaelalexdiaz@users.noreply.github.com>
2022-08-02 20:00:46 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-hi
+var luck = document.querySelector("button");
+
+const options = {
+  method: 'GET',
+  url: 'https://magic-8-ball1.p.rapidapi.com/my_answer/',
+  params: {question: 'I will succeed ?'},
+  headers: {
+    'X-RapidAPI-Key': 'afae7ab92emsh70e7b9a600e0dafp15f82djsn224d672302c4',
+    'X-RapidAPI-Host': 'magic-8-ball1.p.rapidapi.com'
+  }
+}
+
+luck.onclick = function(answer){
+    fetch('https://magic-8-ball1.p.rapidapi.com/my_answer/', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+}
