9f5e7750d191cb1b8ca6f698746312f423abd01d
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-08-04 19:44:44 -0500

    Update script.js

--- 8-Ball API/script.js
+++ 8-Ball API/script.js
@@ -12,20 +12,20 @@ const options = {
   }
 }
 
-luck.onclick = function(answer){
-    fetch('https://magic-8-ball1.p.rapidapi.com/my_answer/', options)
-    console.log(response);
+luck.onclick = function(){
+  fetch('https://magic-8-ball1.p.rapidapi.com/my_answer/', options)
   .then(response => response.json())
-  .then(response => console.log(response))
   .then(data =>{
     var ballResult = data['answer'];
 
     result.innerHTML = ballResult;
   })
-  
-
+  fetch('https://magic-8-ball1.p.rapidapi.com/my_answer/', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
 	.catch(err => console.error(err));
 
 }
+
 
 
